bff0dcd748344ef9ab773a7d2f19542ded951eb6
a1093374 <119493454+a1093374@users.noreply.github.com>
2023-03-02 18:43:30 +0800

    Add files via upload

---
+++ A1093374_0224_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class A1093374_0224_1 {
+    public static void main(String[] argv) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入一個整數：");
+        int num = scanner.nextInt();
+        if (num % 2 == 0) {
+            System.out.println(num + " 是偶數。");
+        } else {
+            System.out.println(num + " 是奇數。");
+        }
+        scanner.close();
+    }
+}
